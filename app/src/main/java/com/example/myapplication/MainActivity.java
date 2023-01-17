4d6289e497859a80735f09bb4b838a459a859024
AyeshaSiddique01 <89693571+AyeshaSiddique01@users.noreply.github.com>
2023-01-18 03:48:35 +0500

    Add feature share via whatsapp and landscap of both pages

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
--- app/src/main/java/com/example/myapplication/results_activity.java
+++ app/src/main/java/com/example/myapplication/results_activity.java
@@ -4,18 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class results_activity extends AppCompatActivity {
     ListView listView;
-    Button goRepository;
+    Button goRepository, shareButton;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -25,7 +28,7 @@ public class results_activity extends AppCompatActivity {
         Intent intent = getIntent();
         listView = findViewById(R.id.Results);
         goRepository = findViewById(R.id.goRepoBtnMain);
-
+        shareButton = findViewById(R.id.sharebtn);
         DbHelper db = new DbHelper(this);
         ArrayList<String> arrayList = db.getResult();
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
@@ -37,6 +40,25 @@ public class results_activity extends AppCompatActivity {
                 Uri webpage = Uri.parse("https://github.com/AyeshaSiddique01/LearningApplicationAssignment/commits/main");
                 Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
                 startActivity(intent);
+            }
+        });
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = "";
+                for (int i = 0; i < arrayList.size(); i++) text += arrayList.get(i) + "\n";
+                PackageManager pm = results_activity.this.getPackageManager();
+                try {
+                    Intent waIntent = new Intent(Intent.ACTION_SEND);
+                    waIntent.setType("text/plain");
+                    PackageInfo info = pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
+                    waIntent.setPackage("com.whatsapp");
+                    waIntent.putExtra(Intent.EXTRA_TEXT, text);
+                    startActivity(Intent.createChooser(waIntent, "Share with"));
+                } catch (PackageManager.NameNotFoundException e) {
+                    Toast.makeText(results_activity.this, "WhatsApp not Installed", Toast.LENGTH_SHORT)
+                            .show();
+                }
             }
         });
     }
